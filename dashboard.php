da39fb48a73937f808c706a687d93c2fa8afea64
LeandroDante LENOVO-DESKTOP <leandrodanteoliveira@hotmail.com>
2020-06-23 14:24:33 -0300

    Limpeza V2 no projeto

--- dashboard.php
+++ dashboard.php
@@ -297,9 +297,6 @@
                 <div class="col">
                   <h3 class="mb-0">Social traffic</h3>
                 </div>
-                <div class="col text-right">
-                  <a href="#!" class="btn btn-sm btn-primary">See all</a>
-                </div>
               </div>
             </div>
             <div class="table-responsive">
