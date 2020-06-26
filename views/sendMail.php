170bf4d13b0453fcb4df1f4ebb19d7b6a42306cd
LeandroDante LENOVO-DESKTOP <leandrodanteoliveira@hotmail.com>
2020-06-26 13:50:35 -0300

    Update sendMail.php

--- views/sendMail.php
+++ views/sendMail.php
@@ -1,4 +1,6 @@
 <?php include 'menu.php'; ?>
+<?php echo '<br><br><br><center><h2>Enviando Email...</h2><h4>Aguarde alguns instantes e será redirecionado!</h4></center>'; ?>
+<div style="display: none;">
 <?php
 iconv_set_encoding("internal_encoding", "UTF-8");
 $to      = $_GET['e'];
@@ -8,7 +10,7 @@ $message = '
 <p>
 Você acaba de receber o convite de uma oportunidade impar, estar acompanhando e participando do beta test da nossa mais recente plataforma!</p>
 <p>
-<h3>Acesse e Descubra! - <a href="http://177.195.154.104/lab2temp/views/register.php">PORTAL2DEV!</a></h3>';
+<h3>Acesse e Descubra! - <a href="http://177.195.154.104/lab2temp/views/register.php">LAB2TEMP!</a></h3>';
 $headers = 'From: portal2dev.lab2dev@gmail.com' . "\r\n" .
     'Reply-To: portal2dev.lab2dev@gmail.com' . "\r\n" .
     'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
@@ -18,3 +20,4 @@ if (mail($to, $subject, utf8_decode($message), $headers)) {
     echo '<script>setTimeout(function(){window.location="profile.php"},500)</script>';
 };
 ?> 
+</div>
